f5d02f8bfa991dbc17758e853659f83490218ffa
hnmhuy <dev.hnmhuy@gmail.com>
2024-10-26 13:18:44 +0700

    Add loggin interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalException } from './exceptions/global_expection';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalException());
-  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalInterceptors(new LoggingInterceptor(),new ResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
